ffaeac04239937d7757488ff9d7e8df5d6cbb0ea
David Szotten <davidszotten@gmail.com>
2013-05-22 19:24:10 +0100

    monkey patch kaiso

--- taal/kaiso.py
+++ taal/kaiso.py
@@ -1,4 +1,7 @@
-from taal import TranslationContextManager, translation_manager
+from __future__ import absolute_import
+
+from taal import (
+    TranslationContextManager, translation_manager, TranslatableString)
 
 
 class TypeTranslationContextManager(TranslationContextManager):
@@ -13,6 +16,42 @@ class TypeTranslationContextManager(TranslationContextManager):
         storage = self.storage
         hierarchy = storage.get_type_hierarchy()
         return (type_[0] for type_ in hierarchy)
+
+    # do i prefer this to monkey-patching?
+    # other options?
+    # def get_type_hierarchy(self, storage):
+        # for type_id, bases, attrs in storage.get_type_hierarchy():
+            # label = TranslatableString(
+                # context=self.context,
+                # message_id=type_id
+            # )
+            # yield (type_id, label, bases, attrs)
+
+
+def monkey_patch_kaiso():
+    from kaiso.persistence import Storage
+
+    get_type_hierarchy = Storage.get_type_hierarchy
+
+    def get_labeled_type_hierarchy(self):
+        for type_id, bases, attrs in get_type_hierarchy(self):
+            label = TranslatableString(
+                context=TypeTranslationContextManager.context,
+                message_id=type_id
+            )
+            yield (type_id, label, bases, attrs)
+
+    Storage.get_type_hierarchy = get_labeled_type_hierarchy
+    monkey_patch_kaiso.get_type_hierarchy = get_type_hierarchy
+
+
+def unpatch_kaiso():
+    from kaiso.persistence import Storage
+    try:
+        Storage.get_type_hierarchy = monkey_patch_kaiso.get_type_hierarchy
+    except AttributeError:
+        # not yet patched
+        pass
 
 
 translation_manager.register(TypeTranslationContextManager)
