6050620ec0b991fed3f2567b861032fb3b167ad8
David Szotten <davidszotten@gmail.com>
2013-07-04 09:22:37 +0100

    bulk updates to non-translated should be allowed

--- tests/test_sqlalchemy.py
+++ tests/test_sqlalchemy.py
@@ -150,6 +150,16 @@ def test_bulk_update_to_none(bound_session):
         bound_session.query(Model).update({'name': None})
 
 
+def test_bulk_update_normal_field(bound_session):
+    # regression test: should be allowed
+    instance = Model(name='name')
+    bound_session.add(instance)
+    bound_session.commit()
+
+    bound_session.query(Model).update({'identifier': 'foo'})
+    bound_session.commit()
+
+
 @pytest.mark.parametrize("initial", [None, 'name', ])
 def test_flushing(bound_session, session_cls, initial):
     instance = Model(name=initial)
